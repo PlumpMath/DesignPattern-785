006e8b827014c74121af1cd4c8e1e47230018ddd
zhzhh <zhihongzhang_hi@163.com>
2015-10-28 22:31:45 +0800

    中介者模式

---
+++ java/mediator/src/main/java/mediator/Client.java
@@ -0,0 +1,18 @@
+package mediator;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mediator m = new President();
+
+		Market market = new Market(m);
+		Development devp = new Development(m);
+		Finacial f = new Finacial(m);
+
+		market.selfAction();
+		market.outAction();
+
+	}
+
+}
---
+++ java/mediator/src/main/java/mediator/President.java
@@ -0,0 +1,22 @@
+package mediator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class President implements Mediator {
+
+	private Map<String, Department>map = new HashMap<String, Department>();
+	
+	@Override
+	public void register(String dname, Department d) {
+		// TODO Auto-generated method stub
+		map.put(dname, d);
+	}
+
+	@Override
+	public void command(String dname) {
+		// TODO Auto-generated method stub
+		map.get(dname).selfAction();
+	}
+
+}
